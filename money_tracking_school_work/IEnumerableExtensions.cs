a332641e2e3b6bf6db4f61062195a1bf65879e37
Nicklas Thor <nicke.tor.86@gmail.com>
2024-02-22 15:06:28 +0100

    Add IEnumerableExtensions.cs that extension method for IEnumerable with WithIndex.
    -Add IEnumerableExtensions.cs that extension method for IEnumerable with WithIndex, that can be used with foreach to get index.
    
    Signed-off-by: Nicklas Thor <nicke.tor.86@gmail.com>

---
+++ money_tracking_school_work/IEnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+namespace money_tracking_school_work
+{
+    using System.Collections.Generic;
+
+    /*
+     * IEnumerableExtensions have extension method for IEnumerable with WithIndex that can be used with foreach to get index
+     * example
+     foreach (var (item, index) in collection.WithIndex())
+        {
+            Debug.WriteLine($"{index}: {item}");
+        }
+     * source https://stackoverflow.com/questions/43021/how-do-you-get-the-index-of-the-current-iteration-of-a-foreach-loop
+    */
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
+           => self.Select((item, index) => (item, index));
+    }
+}
